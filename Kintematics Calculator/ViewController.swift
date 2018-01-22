a331f5475208438cf2d0e76ab29d22666fea67c6
Luke Deratzou <lderatzou@technologys-imac-9762.local>
2018-01-22 08:44:05 -0500

    Initial Commit

---
+++ Kintematics Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kintematics Calculator
+//
+//  Created by Luke Deratzou on 1/22/18.
+//  Copyright © 2018 Luke Deratzou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
